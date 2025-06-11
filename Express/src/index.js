6cb5142a767b7eccb1e5cc712d3d27775e215de0
hannahlbarreto <hannahlbarreto@gmail.com>
2025-06-11 12:34:34 -0400

    Update index.js

--- Express/src/index.js
+++ Express/src/index.js
@@ -1,1 +1,14 @@
+const express = require("express");
 
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Express app iniciada na porta ${PORT}.`);
+});
